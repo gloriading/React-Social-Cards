66b7c5186a896a993670c24bd1b67f69a53e2767
Gloria Ding <gloria.hc.ding@gmail.com>
2018-07-22 15:23:33 -0700

    Updated styles and import logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card'
+import logo01 from './images/smiley01.png';
+import logo02 from './images/smiley02.png';
+import logo03 from './images/smiley03.png';
 
 /*
 import portfileImg from './images/profile.jpg';
@@ -15,23 +18,23 @@ const members = [
   { name: "Amy",
     message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     footer: "amy.com",
-    headline: "Ut enim ad minim veniam",
+    headline: "Which languages I should learn to be a Web Developer?",
     profileImg: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    logoImg: "http://pngimg.com/uploads/perfume/perfume_PNG10323.png"
+    logoImg: logo01
   },
   { name: "Jack",
     message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     footer: "jack.ca",
-    headline: "Ut enim ad minim veniam",
+    headline: "Web Designer or Web Developer?!",
     profileImg: "https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    logoImg: "http://pngimg.com/uploads/globe/globe_PNG58.png"
+    logoImg: logo02
   },
   { name: "Hank",
     message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     footer: "hank.co",
-    headline: "Ut enim ad minim veniam",
+    headline: "Have fun coding!",
     profileImg: "https://images.pexels.com/photos/36479/man-stress-male-face.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    logoImg: "http://pngimg.com/uploads/smiley/smiley_PNG193.png"
+    logoImg: logo03
   }
 ];
 
